88088e2ef0084a5f5992672cdb651325418a4528
alex-k-22 <alexkerzner1@gmail.com>
2023-07-22 16:00:22 -0400

    Tidy up days 13-14

--- day13.py
+++ day13.py
@@ -1,30 +1,18 @@
 import json
-import functools
-from copy import deepcopy
 
 
 def compare_packets(left, right):
     if isinstance(left, int) and isinstance(right, int):
-        if left < right:
-            return True
-        elif left > right:
-            return False
-        else:
-            return None
+        return None if left == right else left < right
+
     elif isinstance(left, list) and isinstance(right, list):
-        while len(left) > 0 and len(right) > 0:
-            left2 = left.pop(0)
-            right2 = right.pop(0)
-            tmp = compare_packets(left2, right2)
+        for left_elt, right_elt in zip(left, right):
+            tmp = compare_packets(left_elt, right_elt)
             if tmp is not None:
                 return tmp
-        if len(left) == 0 and len(right) > 0:
-            return True
-        elif len(left) > 0 and len(right) == 0:
-            return False
-        else:
-            return None
-    else:
+        return None if len(left) == len(right) else len(right) > len(left)
+
+    else:  # Else one is int and one is list
         if isinstance(left, int):
             left = [left]
         else:
@@ -32,30 +20,12 @@ def compare_packets(left, right):
         return compare_packets(left, right)
 
 
-def cmp(left, right):
-    x = compare_packets(deepcopy(left), deepcopy(right))
-    return 1 * (x is False) + 0 * (x is None) - 1 * (x is True)
+with open("input13", "r") as f:
+    all_packets = [json.loads(p) for p in f.readlines() if p != "\n"]
 
+print("Part 1:", sum([idx + 1 for idx, pair in enumerate(zip(all_packets[::2], all_packets[1::2])) if
+                      compare_packets(pair[0], pair[1])]))
 
-if __name__ == "__main__":
-    all_packets = []
-    with open("input13", "r") as f:
-        for p in f.readlines():
-            if p != "\n":
-                all_packets.append(json.loads(p))
-
-    answer_1 = 0
-    for i in range(len(all_packets) // 2):
-        packet_1 = deepcopy(all_packets[2 * i])
-        packet_2 = deepcopy(all_packets[2 * i + 1])
-        if compare_packets(packet_1, packet_2):
-            answer_1 += i + 1
-    print("Part 1 answer is", answer_1)
-
-
-    all_packets.append([[2]])
-    all_packets.append([[6]])
-
-    all_packets.sort(key=functools.cmp_to_key(cmp))
-    answer_2 = (1 + all_packets.index([[2]]))*(1 + all_packets.index([[6]]))
-    print("Part 2 answer is", answer_2)
+loc2 = sum([1 for p in all_packets if compare_packets(p, [[2]])])
+loc6 = sum([1 for p in all_packets if compare_packets(p, [[6]])]) + 1
+print("Part 2:", (1 + loc2) * (1 + loc6))
--- day14.py
+++ day14.py
@@ -1,39 +1,39 @@
+from re import findall
+
+
 class Point:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
+    def __init__(self, coord):
+        self.x = coord[0]
+        self.y = coord[1]
 
 
 def get_input():
     cave = [[False for i in range(700)] for j in range(200)]
     with open("input14", "r") as f:
-        for line in f.readlines():
-            line = line.replace("-", "").replace(">", "").replace(",", " ").split()
-            pairs = [Point(int(line[2*i]), int(line[2*i+1])) for i in range(0, len(line)//2)]
+        lines = map(lambda ell: [int(x) for x in findall("[0-9]+", ell)], f.readlines())
 
-            while len(pairs) > 1:
-                current = pairs.pop(0)
-                if current.x == pairs[0].x:
-                    start = min(current.y, pairs[0].y)
-                    end = max(current.y, pairs[0].y)
-                    for ycoord in range(start, end + 1):
-                        cave[ycoord][current.x] = True
-                else:
-                    start = min(current.x, pairs[0].x)
-                    end = max(current.x, pairs[0].x)
-                    for xcoord in range(start, end + 1):
-                        cave[current.y][xcoord] = True
+    for line in lines:
+        corners = [Point(coord) for coord in zip(line[::2], line[1::2])]
+
+        for idx, c in enumerate(corners[:-1]):
+            if c.x == corners[idx + 1].x:
+                start, end = min(c.y, corners[idx + 1].y), max(c.y, corners[idx + 1].y)
+                for ycoord in range(start, end + 1):
+                    cave[ycoord][c.x] = True
+            else:
+                start, end = min(c.x, corners[idx + 1].x), max(c.x, corners[idx + 1].x)
+                for xcoord in range(start, end + 1):
+                    cave[c.y][xcoord] = True
     return cave
 
 
 def part1():
     cave = get_input()
-
-    ymax = 1 + max([row for row in range(len(cave)) if max(cave[row])])
+    ymax = 1 + next((len(cave) - idx - 1 for idx, row in enumerate(cave[::-1]) if any(row)))
     units = 0
     while True:
         units += 1
-        sand = Point(500, 0)
+        sand = Point((500, 0))
         while True:
             if sand.y == ymax:
                 return units - 1
@@ -52,14 +52,14 @@ def part1():
 
 def part2():
     cave = get_input()
-    ymax = 2 + max([row for row in range(len(cave)) if max(cave[row])])
+    ymax = 2 + next((len(cave) - idx - 1 for idx, row in enumerate(cave[::-1]) if any(row)))
     cave = cave[:ymax + 1]
-    for i in range(len(cave[-1])):
-        cave[-1][i] = True
+    cave[-1] = [True] * len(cave[-1])
+
     units = 0
     while not cave[0][500]:
         units += 1
-        sand = Point(500, 0)
+        sand = Point((500, 0))
         while True:
             if not cave[sand.y + 1][sand.x]:
                 sand.y += 1
@@ -75,6 +75,6 @@ def part2():
     return units
 
 
-print(part1())
-print(part2())
+print("Part 1:", part1())
+print("Part 2:", part2())
 
