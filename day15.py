e933385a00f92e4f4afde1a5b596f805af75d707
alex-k-22 <alexkerzner1@gmail.com>
2023-07-25 13:00:18 -0400

    Complete day 15

--- day15.py
+++ day15.py
@@ -1,5 +1,4 @@
-import re
-from collections import Counter
+from re import findall
 
 
 class Sensor:
@@ -7,7 +6,10 @@ class Sensor:
         self.x = x
         self.y = y
         self.r = r
-        self.bdry = [(x, y+r+1), (x+r+1, y), (x, y-r-1), (x-r-1, y)]  # For part 2
+
+    def bdry(self):
+        return {(self.x + i, self.y + sign * abs(i)) for i in range(-self.r - 1, self.r + 2) for sign in [+1, -1]}
+
 
 class Beacon:
     def __init__(self, x, y):
@@ -15,54 +17,55 @@ class Beacon:
         self.y = y
 
 
-def get_input():
-    sensors = []
-    beacons = []
-    with open("input15", "r") as f:
-        for line in f.readlines():
-            temp = re.findall("\d+", line)
-            temp = [int(t) for t in temp]
-            radius = abs(temp[0] - temp[2]) + abs(temp[1] - temp[3])
-            sensors.append(Sensor(temp[0], temp[1], radius))
-            beacons.append(Beacon(temp[2], temp[3]))
-    return sensors, set(beacons)
+with open("input15", "r") as f:
+    lines = [[int(x) for x in findall("[-]*[0-9]+", line)] for line in f.readlines()]
+
+sensors = {Sensor(line[0], line[1], abs(line[0] - line[2]) + abs(line[1] - line[3])) for line in lines}
+beacons = {Beacon(line[2], line[3]) for line in lines}
+
+
+def covered_range(sensor, row_number, part):
+    available_radius = sensor.r - abs(sensor.y - row_number)
+    if available_radius < 0:
+        return None
+    lower = max(0, sensor.x - available_radius) if part == 2 else sensor.x - available_radius
+    upper = min(4000000, sensor.x + available_radius) if part == 2 else sensor.x + available_radius
+    return [lower, upper]
 
 
 def part1():
-    sensors, beacons = get_input()
-    beacon_x_pts = set([b.x for b in beacons if b.y == 2000000])
-    pts_within_radii = []
-    for s in sensors:
-        if abs(s.y - 2000000) < s.r:
-            r_avail = s.r - abs(s.y - 2000000)
-            pts_within_radii += [s.x + i for i in range(-r_avail, r_avail + 1)]
-            if r_avail == 0:
-                pts_within_radii.append(s.x)
-    return len(set(pts_within_radii) - beacon_x_pts)
+    total_covered = 0
+    row = 2000000
+    beacon_locations = {b.x for b in beacons if b.y == 2000000}
+    intervals = sorted([covered_range(sensor, row, part=1) for sensor in sensors if covered_range(sensor, row, part=1)], key=lambda x: x[0])
+    ivl = intervals[0]
+    for e in intervals:
+        if e[0] <= ivl[1] + 1:
+            ivl[1] = max(ivl[1], e[1])
+        else:
+            total_covered += ivl[1] - ivl[0] + 1 - sum([1 for x in beacon_locations if ivl[0] <= x <= ivl[1]])
+            ivl = e
+    total_covered += ivl[1] - ivl[0] + 1 - sum([1 for x in beacon_locations if ivl[0] <= x <= ivl[1]])
+    return total_covered
 
 
 def part2():
-    sensors, beacons = get_input()
-    candidates = []
-    for s in sensors:
-        for i in range(-s.r - 1, s.r + 2):
-            if 0 <= s.x + i <= 4000000 and 0 <= s.y + s.r + 1 - i <= 4000000:
-                candidates.append((s.x + i, s.y + (s.r + 1 - i)))
-        print(len(candidates))
-    ctr = Counter(candidates)
-    for c in ctr:
-        if ctr[c] == 4:
-            return c
+    for row in range(4000000):
+        ivl = [-1, -1]
+        for e in sorted([covered_range(sensor, row, part=2) for sensor in sensors if covered_range(sensor, row, part=2)],
+                        key=lambda x: x[0]):
+            if e[0] > ivl[1] + 1:
+                return 4000000 * (ivl[1] + 1) + row
+            ivl[1] = max(ivl[1], e[1])
+        if ivl[1] != 4000000:
+            return 4000000 ** 2 + 4000000
+
+
+print("Part 1:", part1())
+print("Part 2:", part2())
 
 
 
-def sensor_intersection(s1, s2):
-    pts = []
-    for i in [0, 1, 2]:
 
-
-
-#print(part1())
-print(part2())
 
 
